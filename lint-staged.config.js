d0e1e2e348bdff2743389e28152aa42f74919328
delt4d <leandrocruz717@gmail.com>
2023-11-10 00:28:36 -0300

    chore(lint-staged.config.js): test before lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,8 +2,8 @@ module.exports = {
     '**/*.(ts|tsx|js)': (filenames) => {
         return [
             'npx tsc --noEmit',
-            'npm run lint',
             `npm run test -- --bail --findRelatedTests ${filenames.join(' ')}`,
+            'npm run lint',
         ]
     },
 }
